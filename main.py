75b91ff8689f14d578de0cd7d4cd7b3d226f9e1b
VarunUpadhyayShorthillsAI <varun.upadhyay@shorthills.ai>
2025-04-09 12:23:56 +0530

    learning - 1

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-name = input("Hey bud what's your name")
+# Definition and Usage
+# The enumerate() function takes a collection (e.g. a tuple) and returns it as an enumerate object.
+
+# The enumerate() function adds a counter as the key of the enumerate object.
+
+# Syntax
+# enumerate(iterable, start)
+
+
+x = ('Man' , 'Woman' , 'Others')
+y = enumerate(x)
+print(y)
+new_format = list(y)
+print(new_format)
+
+
+
+#to open any file 
+file = open(test.py)
